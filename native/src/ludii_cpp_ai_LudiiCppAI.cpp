4171439ddcdd077a7cffd52029d7312177ba37fc
DennisSoemers <d.soemers@gmail.com>
2022-12-09 14:55:08 +0100

    compiling native code and building lib

---
+++ native/src/ludii_cpp_ai_LudiiCppAI.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "ludii_cpp_ai_LudiiCppAI.h"
+
+JNIEXPORT jobject JNICALL Java_ludii_1cpp_1ai_LudiiCppAI_nativeSelectAction
+	(JNIEnv* jenv, jobject jobjAI, jobject game, jobject context, jdouble maxSeconds, 
+	jint maxIterations, jint maxDepth)
+	{
+		std::cout << "native implementation for selectAction()!" << std::endl;
+		return nullptr;
+	}
+
+
+JNIEXPORT void JNICALL Java_ludii_1cpp_1ai_LudiiCppAI_nativeInitAI
+  (JNIEnv* jenv, jobject jobjAI, jobject game, jint playerID)
+  {
+	  std::cout << "native implementation for initAI()!" << std::endl;
+  }
+
+
+JNIEXPORT void JNICALL Java_ludii_1cpp_1ai_LudiiCppAI_nativeCloseAI
+  (JNIEnv* jenv, jobject jobjAI)
+  {
+	  std::cout << "native implementation for closeAI()!" << std::endl;
+  }
